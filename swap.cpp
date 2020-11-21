5f3c005901c48200084d387a40215fd3281a753e
Karan8779 <71130973+Karan8779@users.noreply.github.com>
2020-11-21 20:59:57 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2, temp;
+
+    cout<< "enter the numbers to be swapped : \n";
+    cin>>num1>>num2;
+    cout << "Before swapping." << endl;
+    cout << "num1 = " << num1 << endl ;
+    cout << "num2 = " << num2 << endl;
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "num1 = " << num1 << endl;
+    cout << "num2 = " << num2 << endl;
+    
+
+    return 0;
+}

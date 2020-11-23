a13ed884c5aa32eafbbc36e4f634b8485ba5603f
Karan8779 <71130973+Karan8779@users.noreply.github.com>
2020-11-23 10:45:45 +0530

    Add files via upload

---
+++ uen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int a;
+
+    cout << "Enter the number: ";
+    cin >> a;
+
+    cout << "The number entered is: " << a;    
+    return 0;
+}
